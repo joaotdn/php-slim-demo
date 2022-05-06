39c78b44749757252744e28be874eef168fcba29
joao teodoro <joaotdn@gmail.com>
2022-05-05 21:31:06 -0300

    hello world

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
-$app =  new \Slim\App();
+$app = new \Slim\App();
+
+$app->get('/', function($request, $response, $args) {
+    return $response->getBody()->write('Hello World!');
+});
+
 $app->run();
