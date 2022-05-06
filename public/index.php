fa93d748ceb8a7fef0bf80a061a0d418e2d9183f
joao teodoro <joaotdn@gmail.com>
2022-05-05 21:44:18 -0300

    firt get request

--- public/index.php
+++ public/index.php
@@ -1,11 +1,17 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require_once "../vendor/autoload.php";
 
 $app = new \Slim\App();
 
-$app->get('/', function($request, $response, $args) {
-    return $response->getBody()->write('Hello World!');
+$app->get('/produtos[/nome]', function(Request $request, Response $response, array $args) {
+    // Caso o limite seja nulo, o valor padrão será 10
+    $limit = $request->getQueryParams()['limit'] ?? 10;
+    $nome = $args['nome'] ?? 'mouse';
+    return $response->getBody()
+        ->write("{$limit} produtos do banco de dados com o nome {$nome}");
 });
 
 $app->run();
