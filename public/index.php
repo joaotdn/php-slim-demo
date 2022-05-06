bbebf4239eafe208328f9584edcc3703156f0f62
joao teodoro <joaotdn@gmail.com>
2022-05-05 22:03:22 -0300

    first post request

--- public/index.php
+++ public/index.php
@@ -6,12 +6,19 @@ require_once "../vendor/autoload.php";
 
 $app = new \Slim\App();
 
-$app->get('/produtos[/nome]', function(Request $request, Response $response, array $args) {
+$app->get('/produto[/nome]', function(Request $request, Response $response, array $args) {
     // Caso o limite seja nulo, o valor padrão será 10
     $limit = $request->getQueryParams()['limit'] ?? 10;
     $nome = $args['nome'] ?? 'mouse';
+
     return $response->getBody()
         ->write("{$limit} produtos do banco de dados com o nome {$nome}");
+});
+
+$app->post('/produto', function(Request $request, Response $response, array $args) {
+    $data = $request->getParsedBody();
+    $nome = $data['nome'] ?? '';
+    return $response->getBody()->write("Produto {$nome}");
 });
 
 $app->run();
